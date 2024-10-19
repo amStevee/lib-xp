9c2c502e5f18927a977e20f51c5414deac7ce950
amStevee <stevenanongo2@gmail.com>
2024-10-19 11:15:53 -0700

    add default options to GMD field in book table

--- server/src/routes/auth-routes/auth.routes.ts
+++ server/src/routes/auth-routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signin, signin_google, signout } from '../../controllers/user/auth.controller';
+import { signin, signin_google, signout } from '../../controllers/auth/auth.controller';
 import passport from 'passport';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.route('/signin').post(signin);
 router.route('/google').get(passport.authenticate('google'));
 router.route('/redirect/google').get(signin_google);
 router.route('/signout').get(signout);
+
 
 
 export default router;

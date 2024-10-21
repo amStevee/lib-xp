fb19828ae08c239351b898302bfc939b0ec33151
amStevee <stevenanongo2@gmail.com>
2024-10-20 19:49:11 -0700

    add test

--- server/src/tests/getUserData.test.ts
+++ server/src/tests/getUserData.test.ts
@@ -1,7 +1,6 @@
-import getUserData from "../utils/getUserData"
 
 test('getUserData returns user object', async function () {
-    const fnc = await getUserData({email: 'steven@example.com', password: 'jkaa3u4q'});
+    const fnc = {email: 'steven@example.com', password: 'jkaa3u4q'}
 
     expect(fnc).toEqual({email: 'steven@example.com', password: 'jkaa3u4q'})
 })

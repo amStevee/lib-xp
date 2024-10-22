9ebe37c85dbbb1b26ff3868fc2a5c4827f7435cb
amStevee <stevenanongo2@gmail.com>
2024-10-21 18:25:19 -0700

    add maxAge to cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors({
     origin: '*',
     credentials: true,
-    maxAge: 60 * 1000
+    maxAge: 24 * 60 * 60 * 1000
 }));
 app.use(limiter);
 passportMiddleware();

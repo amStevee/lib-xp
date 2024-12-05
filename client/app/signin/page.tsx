9101fcedba856a45c13231f7f18e5965acb74308
amStevee <stevenanongo2@gmail.com>
2024-12-05 07:36:44 -0800

    solve the &aps; error

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -18,17 +18,22 @@ export default function Home() {
 
   async function handleSignin(formState:FormSate) {
     console.log(formState)
-    await axios.post('/oauth2/signin', JSON.stringify(formState))
+    try {
+      await axios.post('/oauth2/signin', JSON.stringify(formState))
+    } catch (error) {
+      
+    }
   }
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
+    // there is something wrong with the top marging/padding while rendring
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center mt-40 min-h-screen p-8 gap-16 sm:p-8 font-[family-name:var(--font-geist-sans)] ">
       <form ref={formRef} className="flex flex-col gap-4 border-rose-950 border-2 p-8">
         <FormControl required>
             <InputLabel htmlFor="email">Email address</InputLabel>
             <Input id="email" name='email' aria-describedby="my-helper-text" onChange={e => changeFormState(e)} />
-            <FormHelperText id="my-helper-text">We&aps;ll never share your email.</FormHelperText>
+            <FormHelperText id="my-helper-text">We will never share your email.</FormHelperText>
         </FormControl>
         <FormControl required>
             <InputLabel htmlFor="password">Password</InputLabel>

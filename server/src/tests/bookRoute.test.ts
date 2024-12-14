2b7025ea47171331a2497b58179906a2ada16676
amStevee <stevenanongo2@gmail.com>
2024-12-14 15:15:09 -0800

    debugging test

--- server/src/tests/bookRoute.test.ts
+++ server/src/tests/bookRoute.test.ts
@@ -1,13 +1,5 @@
 test('Get all books route', async function () {
-    const url = process.env.API_URL || 'http://localhost:8000'
-    try {
-        const data = await fetch(`${url}/books`);
-        expect(data).toEqual({
-            "msg": "success",
-            "data": []
-          })
-    } catch (error:any) {
-        return error.message
-    }
+    const url = process.env.API_URL || 'http://localhost:3010'
+    expect(url).toEqual('http://localhost:3010')
     
 })

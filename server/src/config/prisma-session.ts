b244f7b8a2edfea034a1de790ddb98e585bf5416
amStevee <stevenanongo2@gmail.com>
2024-12-18 12:29:14 -0800

    debuging prisma-session

--- server/src/config/prisma-session.ts
+++ server/src/config/prisma-session.ts
@@ -2,17 +2,18 @@ import expressSession from 'express-session';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 
 import db from './prismaClient';
-import dotenv from 'dotenv';
 import { CustomError } from '../utils/errorHandler';
+import dotenv from 'dotenv';
 dotenv.config()
 
-if (!process.env.SESSION_SECRET)  throw new CustomError('session secret not provided', 403);
+
+
 
 const Session = expressSession({
     cookie: {
      maxAge: 7 * 24 * 60 * 60 * 1000 // ms
     },
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || '',
     resave: true,
     saveUninitialized: true,
     store: new PrismaSessionStore(

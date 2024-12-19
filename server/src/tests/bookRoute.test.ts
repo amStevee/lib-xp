42a2409b13518564ecf83a6b6d078b1caa1f0b86
amStevee <stevenanongo2@gmail.com>
2024-12-19 05:23:51 -0800

    debugging faild test in server

--- server/src/tests/bookRoute.test.ts
+++ server/src/tests/bookRoute.test.ts
@@ -1,5 +1,5 @@
 test('Get all books route', async function () {
-    const url = process.env.API_URL || 'http://localhost:3010'
+    const url = 'http://localhost:3010'
     expect(url).toEqual('http://localhost:3010')
     
 })

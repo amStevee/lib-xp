06a8a329ee7067b1833b6828a231c8182f25b531
amStevee <stevenanongo2@gmail.com>
2024-12-19 05:01:11 -0800

    debugging authController

--- server/src/utils/verifyClientData.ts
+++ server/src/utils/verifyClientData.ts
@@ -3,13 +3,14 @@ import { CustomError } from "./errorHandler";
 export function crossCheckClientData<T extends {}>(reqBody:T, required = {}) {
 
     if (!Object.keys(reqBody).length) 
-        return new CustomError(`must provied data to request body`, 400);
+        return new CustomError(`must provied data to request`, 400);
 
     let emptyFildArray = [];
     // Check for falsy data in filds.
     for (const key in reqBody) {
         if (!reqBody[key as keyof typeof reqBody] || reqBody[key as keyof typeof reqBody] === undefined) emptyFildArray.push(key);
     }
+    
     //  Check to make sure required filds are present.
     for (const key in required) {
         if (!reqBody[key as keyof typeof required]) {
@@ -18,6 +19,7 @@ export function crossCheckClientData<T extends {}>(reqBody:T, required = {}) {
             }
         };
     }
+
     
     if (emptyFildArray.length) return new CustomError(`must provied ${emptyFildArray.toString()}`, 400);
 

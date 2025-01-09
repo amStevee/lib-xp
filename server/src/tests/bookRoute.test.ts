05f12a7551b1c0ece75db212a172bb508686b54c
amStevee <stevenanongo2@gmail.com>
2025-01-09 06:32:31 -0800

    testing book route

--- server/src/tests/bookRoute.test.ts
+++ server/src/tests/bookRoute.test.ts
@@ -1,5 +1,8 @@
+
+
 test('Get all books route', async function () {
     const url = 'http://localhost:3010'
-    expect(url).toEqual('http://localhost:3010')
+    const getAllBooks = await fetch('http://localhost:3010/books');
+    expect(getAllBooks).toBeFalsy()
     
 })

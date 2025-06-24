d5f93eb3fd9389c637b8b789681a104d5b4df0c7
amStevee <stevenanongo2@gmail.com>
2025-06-23 19:03:49 -0700

    debugging prisma

--- server/src/tests/getUserData.test.ts
+++
@@ -1,6 +0,0 @@
-
-test('getUserData returns user object', async function () {
-    const fnc = {email: 'steven@example.com', password: 'jkaa3u4q'}
-
-    expect(fnc).toEqual({email: 'steven@example.com', password: 'jkaa3u4q'})
-})
